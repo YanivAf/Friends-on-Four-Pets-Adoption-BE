a22a1a4b700e16cb2cf656a991e73b180b6e1a39
Yaniv Aflalo <yanivaflalo@gmail.com>
2021-12-31 15:54:27 +0200

    trying to change httpOnly to false

--- src/server.ts
+++ src/server.ts
@@ -22,6 +22,7 @@ app.use(
     cookie: {
       sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
       secure: process.env.NODE_ENV === "production",
+      httpOnly: false,
     }
   })
 );
